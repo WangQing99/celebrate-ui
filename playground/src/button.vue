b113690500d4b1cda71dc91131c625ad5a4b7c54
WQ <wq990217@gmail.com>
2022-08-01 10:15:33 +0800

    feat:init project

---
+++ playground/src/button.vue
@@ -0,0 +1,10 @@
+<script setup lang='ts'>
+
+</script>
+
+<template>
+	<button>按钮</button>
+</template>
+
+<style scoped lang='scss'>
+</style>
---
+++ playground/vite.config.ts
@@ -0,0 +1,7 @@
+import Vue from "@vitejs/plugin-vue"
+import { defineConfig } from 'vite'
+import './vite.init'
+
+export default defineConfig({
+	plugins: [Vue()],
+})
